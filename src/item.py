65cbd88b33adb471883c1f662959a227b0d43945
Vitaliy Petrenko <mail@v-petrenko.ru>
2023-05-27 01:24:14 +0300

    homework #2 complete

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +17,20 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            raise Exception('Длина наименования товара превышает 10 символов.')
+        self.__name = name
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +45,20 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= Item.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """инициализирует экземпляры `Item` данными из файла _src/items.csv_"""
+        file_path = os.path.join('..', 'src', 'items.csv')
+        if not os.path.exists(file_path):
+            file_path = os.path.join('src', 'items.csv')  # Если запускаемся на уровень выше...
+        with open(file_path, encoding='cp1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(string: str) -> int:
+        if string.isdigit():
+            return int(string)
+        return int(float(string))
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,13 +1,21 @@
 """Здесь тесты с использованием pytest для модуля item."""
-
+import pytest
 from src.item import Item
 
 
 def test_init():
     item = Item('test', 200.0, 100)
     Item.pay_rate = 0.5
-    assert item.name == 'test'
+    item.name = "test2"
+    with pytest.raises(Exception):
+        item.name = "1234567890l"
+    assert item.name == 'test2'
     assert item.price == 200.0
     assert item.quantity == 100
     item.apply_discount()
     assert item.calculate_total_price() == 10000.0
+
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 6
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.5') == 5
